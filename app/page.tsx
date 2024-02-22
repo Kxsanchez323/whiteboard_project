fbb1b6ef87d0fd0e3f198c81971d82fd41773399
Kevin Sanchez <Chilledsonic.ks2@gmail.com>
2024-02-21 21:15:08 -0500

    Began Sidebars

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
 "use client";
 
 import Live from "@/components/Live";
+import Navbar from "@/components/Navbar";
 
 export default function Page() {
   return (
-    <div>
-       <Live />
-    </div>
+    <main className="h-screen overflow-hidden">
+      <Navbar />
+      <section className="flex h-full flew-row">
+      <LeftSidebar />
+      <Live />
+      <RightSidebar />
+      </section>
+       
+    </main>
 
   );
 }
